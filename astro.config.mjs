d8a3c79a297e8aa3f4855692fcdb368ef8edb362
sulphite <mikanchik@gmail.com>
2024-07-17 11:27:37 +0100

    configure for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,10 @@ import sanity from "@sanity/astro";
 // https://astro.build/config
 export default defineConfig({
   // uncomment for gh pages
-  site: "https://tt01924.github.io",
-  base: "Sara-Biatchinyi-Website"
+  // site: "https://tt01924.github.io",
+  // base: "Sara-Biatchinyi-Website"
 
-  // site: "https://sarabiatchinyi.com"
+  site: "https://sarabiatchinyi.com"
   ,
   integrations: [sanity({
     projectId: 'k6u6ik8n',
